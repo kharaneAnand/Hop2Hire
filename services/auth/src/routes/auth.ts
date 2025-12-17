ed1462127dd32ff36f6a13f8765022bb99f6c111
kharaneAnand <anandkharane@gmail.com>
2025-12-17 16:24:29 +0530

    multer middlerware setup is done and working on the registerUser

--- services/auth/src/routes/auth.ts
+++ services/auth/src/routes/auth.ts
@@ -1,7 +1,8 @@
 import express from 'express'
 import { registerUser } from '../controller/auth.js';
+import uploadFile from '../middleware/multer.js';
 
 const router = express.Router() ;
 
-router.post("/register" , registerUser) ;
+router.post("/register" ,uploadFile , registerUser) ;
 export default router ;
