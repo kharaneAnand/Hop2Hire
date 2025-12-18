b02114cc2cac6546b640536915ad1494210c9728
kharaneAnand <anandkharane@gmail.com>
2025-12-18 18:35:15 +0530

    for resume upload multer and cloudinary setup is done , upload service created under the utils folder and RegisterUser is completed

--- services/auth/src/controller/auth.ts
+++ services/auth/src/controller/auth.ts
@@ -1,7 +1,9 @@
+import getBuffer from "../utils/buffer.js";
 import { sql } from "../utils/db.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import { TryCatch } from "../utils/TryCatch.js";
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcrypt' ;
+import axios from "axios";
 
 export const registerUser =TryCatch(async(req , res , next)=>{
     const {name , email , password , phoneNumber , role , bio} = req.body ;
@@ -29,11 +31,26 @@ export const registerUser =TryCatch(async(req , res , next)=>{
 
     } else if(role==="jobseeker"){
         const file  = req.file ;
-         const [user] = await sql`INSERT INTO users(name  , email , password , phone_number , role , resume , bio) VALUES 
-        (${name} , ${email} , ${hashPassword} , ${phoneNumber} , ${role}) RETURNING 
-        user_id , name , email , phone_number , role , created_at` ;
-        
 
+        if(!file){
+            throw new ErrorHandler(400 , "Resume file is Required  for jobseekers , please upload your Resume !") ;
+        }
+
+        const fileBuffer = getBuffer(file) ;
+        if(!fileBuffer){
+            throw new ErrorHandler(500 , "Failed to generate a Buffer") ;
+        }
+
+
+        const {data} = await axios.post(
+            `${process.env.UPLOAD_SERVICE}/api/utils/upload` ,
+             {buffer : fileBuffer.content}
+            ); 
+
+         const [user] = await sql`INSERT INTO users(name  , email , password , phone_number , role , bio , resume  ,resume_public_id ) VALUES 
+        (${name} , ${email} , ${hashPassword} , ${phoneNumber} , ${role} , ${bio} , ${data.url} , ${data.public_id}) RETURNING 
+        user_id , name , email , phone_number , role ,bio , resume , created_at` ;
+        
     }
 
     res.json(email) ;
---
+++ services/utils/src/index.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import routes from './routes.js';
+import cors from 'cors' ;
+import { v2 as cloudinary } from 'cloudinary' 
+
+
+dotenv.config() ;
+
+cloudinary.config({
+    cloud_name : process.env.CLOUD_NAME ,
+    api_key : process.env.API_KEY ,
+    api_secret : process.env.API_SECRET ,
+})
+
+
+const app = express() ;
+
+
+
+app.use(cors()) ;
+app.use(express.json({limit:"100mb"})) 
+app.use(express.urlencoded({limit:"100mb" , extended: true}))
+app.use("/api/utils" , routes) ;
+
+app.listen(process.env.PORT , ()=>{
+    console.log(`utils service is running on the http://localhost:${process.env.PORT}`) ;
+})
