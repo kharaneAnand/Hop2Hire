77993aaf50b8eb46bbc71e4616a56a868a622f28
kharaneAnand <anandkharane@gmail.com>
2025-12-19 21:19:52 +0530

    feat(auth): finalize secure reset password flow with JWT and Redis

--- services/auth/src/routes/auth.ts
+++ services/auth/src/routes/auth.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import { forgotPassword, loginUser, registerUser } from '../controller/auth.js';
+import { forgotPassword, loginUser, registerUser, resetPassword } from '../controller/auth.js';
 import uploadFile from '../middleware/multer.js';
 
 const router = express.Router() ;
@@ -7,4 +7,5 @@ const router = express.Router() ;
 router.post("/register" ,uploadFile , registerUser) ;
 router.post("/login" , loginUser) ;
 router.post("/forgot", forgotPassword) ;
+router.post("/reset/:token" , resetPassword) ;
 export default router ;
