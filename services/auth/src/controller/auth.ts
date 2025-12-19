62c0143c4c114ba1f7cc21bba1da7726e1645275
kharaneAnand <anandkharane@gmail.com>
2025-12-19 16:15:13 +0530

    send forgot password reset email via Kafka

--- services/auth/src/controller/auth.ts
+++ services/auth/src/controller/auth.ts
@@ -5,6 +5,9 @@ import { TryCatch } from "../utils/TryCatch.js";
 import bcrypt from 'bcrypt' ;
 import axios from "axios";
 import jwt from 'jsonwebtoken' ;
+import { forgotPasswordTemplate } from "../templet.js";
+import { publishToTopic } from "../producer.js";
+import { error } from "console";
 
 export const registerUser =TryCatch(async(req , res , next)=>{
     const {name , email , password , phoneNumber , role , bio} = req.body ;
@@ -109,3 +112,45 @@ export const loginUser = TryCatch(async(req , res , next)=>{
         });
 });
 
+export const forgotPassword = TryCatch(async(req , res , next)=>{
+
+    const {email} = req.body ;
+
+    if(!email){
+        throw new ErrorHandler(400 , "email is required") ;
+    }
+
+    const users = await sql`SELECT user_id , email FROM users WHERE email = ${email}` ;
+    if(users.length ===0) {
+        return res.json({
+            message : "If that email exists , we have sent a reset link" ,
+        });
+    }
+
+    const user = users[0] ;
+
+    const resetToken = jwt.sign({
+        email : user.email , type:"reset" ,
+    }, 
+    process.env.JWT_SEC as string,
+    {
+        expiresIn:"15m"
+    }
+);
+
+    const resetLink = `${process.env.Frontend_Url}/reset/${resetToken}`
+
+    const message = {
+        to:email,
+        subject:"Reset your password - Hope2Hire",
+        html : forgotPasswordTemplate(resetLink),
+    };
+
+    publishToTopic("send-mail" , message).catch((error)=>{
+        console.error("failed to send message" , error) ;
+    }) ;
+
+    res.json({
+        message:"If that email exists , we have sent a reset link " ,
+    });
+});
--- services/utils/src/consumer.ts
+++ services/utils/src/consumer.ts
@@ -32,8 +32,8 @@ export const startSendMailConsumer = async()=>{
                   port :465 ,
                   secure:true ,
                   auth:{
-                    user:"xyz",
-                    pass:"yzx",
+                    user:process.env.SMTP_USER,
+                    pass:process.env.SMTP_PASS,
                   }
                   });
 
