01a1d256dc038669ebceed9abaeade9e48003893
kharaneAnand <anandkharane@gmail.com>
2025-12-20 14:16:45 +0530

    feat(user): add updateUserProfile controller

--- services/user/src/routes/user.ts
+++ services/user/src/routes/user.ts
@@ -1,8 +1,9 @@
 import express from 'express' 
 import { isAuth } from '../middleware/auth.js';
-import { getUserProfile, myProfile } from '../controller/user.js';
+import { getUserProfile, myProfile, updateUserProfile } from '../controller/user.js';
 
 const router = express.Router() ;
 router.get("/me" , isAuth , myProfile) ;
 router.get("/:userId" , isAuth , getUserProfile) ;
+router.put("/update/profile" , isAuth , updateUserProfile) ;
 export default router;
