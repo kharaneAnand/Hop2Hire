bd67240ca14000cb1c59b0b85e561d97a1a74c4b
kharaneAnand <anandkharane@gmail.com>
2025-12-20 13:54:49 +0530

    feat(user): add myProfile and getUserProfile controllers

--- services/user/src/middleware/auth.ts
+++ services/user/src/middleware/auth.ts
@@ -45,15 +45,27 @@ export const isAuth = async(req : AuthenticatedRequest, res:Response , next: Nex
             return ;
         }
 
-        const users = await sql `
-            SELECT u.user_id , u.name , u.email , u.phone_number , u.role , u.bio , u.resume , u.resume_public_id , u.profile_pic ,
-            u.profile_pic_public_id , u.subscription ,
-            ARRAY_AGG(s.name) FILTER (WHERE s.name IS NOT NULL) as skills 
-            FROM users u LEFT JOIN user_skills us ON u.user_id = us.user_id 
-            LEFT JOIN skills s ON us.skills_id = s.skills_id 
-            WHERE u.user_id = ${decodedPayload.id} 
-            GROUP BY u.user_id ;
-        `;
+       const users = await sql`
+    SELECT 
+        u.user_id,
+        u.name,
+        u.email,
+        u.phone_number,
+        u.role,
+        u.bio,
+        u.resume,
+        u.resume_public_id,
+        u.profile_pic,
+        u.profile_pic_public_id,
+        u.subscription,
+        ARRAY_AGG(s.name) FILTER (WHERE s.name IS NOT NULL) AS skills
+    FROM users u
+    LEFT JOIN user_skills us ON u.user_id = us.user_id
+    LEFT JOIN skills s ON us.skill_id = s.skill_id
+    WHERE u.user_id = ${decodedPayload.id}
+    GROUP BY u.user_id;
+`;
+
 
         if(users.length === 0){
             res.status(400).json({
--- services/user/src/routes/user.ts
+++ services/user/src/routes/user.ts
@@ -1,5 +1,8 @@
 import express from 'express' 
+import { isAuth } from '../middleware/auth.js';
+import { getUserProfile, myProfile } from '../controller/user.js';
 
 const router = express.Router() ;
-
+router.get("/me" , isAuth , myProfile) ;
+router.get("/:userId" , isAuth , getUserProfile) ;
 export default router;
