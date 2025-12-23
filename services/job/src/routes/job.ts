9e269ff7373b2674043ea627af8c2e19a249c4bf
kharaneAnand <anandkharane@gmail.com>
2025-12-23 13:59:53 +0530

    feat(jobs) :- add jobs and update jobs API with the recruiter ownership

--- services/job/src/routes/job.ts
+++ services/job/src/routes/job.ts
@@ -1,12 +1,13 @@
 import express from "express";
 import { isAuth } from "../middleware/auth.js";
 import uploadFile from "../middleware/multer.js";
-import { createCompany, createJob, deleteCompany } from "../controller/job.js";
+import { createCompany, createJob, deleteCompany, updateJob } from "../controller/job.js";
 
 const router = express.Router() ;
 
 router.post("/company/new" , isAuth , uploadFile , createCompany) ;
 router.delete("/company/:companyId" , isAuth , deleteCompany) ;
 router.post("/new" , isAuth , createJob) ;
+router.put("/:jobId" , isAuth , updateJob) ;
 
 export default router ;
