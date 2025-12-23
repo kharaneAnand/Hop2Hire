7117a0d4b5d050ae4837c2531640d5e802184e5b
kharaneAnand <anandkharane@gmail.com>
2025-12-23 12:55:26 +0530

    feat(job) : add create company API with the recruiter - only access

--- services/job/src/controller/job.ts
+++ services/job/src/controller/job.ts
@@ -74,3 +74,84 @@ export const deleteCompany = TryCatch(async(req:AuthenticatedRequest , res , nex
      });
 
 });
+
+export const createJob = TryCatch(async (req: AuthenticatedRequest, res, next) => {
+  const user = req.user;
+
+  if (!user) {
+    throw new ErrorHandler(401, "Authentication Required!");
+  }
+
+  if (user.role !== "recruiter") {
+    throw new ErrorHandler(403, "Forbidden: only recruiter can create a job");
+  }
+
+  const {
+    title,
+    description,
+    salary,
+    location,
+    role,
+    job_type,
+    work_location,
+    company_id,
+    openings,
+  } = req.body;
+
+  if (
+    !title ||
+    !description ||
+    !salary ||
+    !location ||
+    !role ||
+    !job_type ||
+    !work_location ||
+    !openings
+  ) {
+    throw new ErrorHandler(400, "All fields are required!");
+  }
+
+  const [company] = await sql`
+    SELECT company_id 
+    FROM companies 
+    WHERE company_id = ${company_id}
+    AND recruiter_id = ${user.user_id}
+  `;
+
+  if (!company) {
+    throw new ErrorHandler(404, "Company not found");
+  }
+
+  const [newJob] = await sql`
+    INSERT INTO jobs (
+      title,
+      description,
+      salary,
+      location,
+      role,
+      job_type,
+      work_location,
+      company_id,
+      posted_by_recruiter_id,
+      openings
+    )
+    VALUES (
+      ${title},
+      ${description},
+      ${salary},
+      ${location},
+      ${role},
+      ${job_type},
+      ${work_location},
+      ${company_id},
+      ${user.user_id},
+      ${openings}
+    )
+    RETURNING *
+  `;
+
+  res.status(201).json({
+    message: "✅ Job posted successfully",
+    job: newJob,
+  });
+});
--- services/job/src/routes/job.ts
+++ services/job/src/routes/job.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import { isAuth } from "../middleware/auth.js";
 import uploadFile from "../middleware/multer.js";
-import { createCompany, deleteCompany } from "../controller/job.js";
+import { createCompany, createJob, deleteCompany } from "../controller/job.js";
 
 const router = express.Router() ;
 
 router.post("/company/new" , isAuth , uploadFile , createCompany) ;
 router.delete("/company/:companyId" , isAuth , deleteCompany) ;
+router.post("/new" , isAuth , createJob) ;
 
 export default router ;
